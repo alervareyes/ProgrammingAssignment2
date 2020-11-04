5ac49fc92d774729693ecd581771f162e33a98e0
alervareyes <73333390+alervareyes@users.noreply.github.com>
2020-11-04 21:11:04 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  # Set function
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(n) {
+    m <<- n
+  }
+  
+  getInverse <- function() m
+  
+  list(
+    set = set,
+    get = get,
+    setInverse = setInverse,
+    getInverse = getInverse
+  )
 }
 
 
